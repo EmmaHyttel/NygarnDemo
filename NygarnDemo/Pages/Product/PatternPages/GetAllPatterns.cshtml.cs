9758f0ab53cf08ea984b51522ae496a07efa3bbc
Emma Hyttel <emhyttel@gmail.com>
2024-04-23 13:58:32 +0200

    Rettelse i GetAllPatterns

--- NygarnDemo/Pages/Product/PatternPages/GetAllPatterns.cshtml.cs
+++ NygarnDemo/Pages/Product/PatternPages/GetAllPatterns.cshtml.cs
@@ -1,9 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using NygarnDemo.MockData;
 using NygarnDemo.Services.Interfaces;
-using NygarnDemo.Services.ProductServices;
-using System.Security.Cryptography.X509Certificates;
 
 namespace NygarnDemo.Pages.Product.PatternPages
 {
