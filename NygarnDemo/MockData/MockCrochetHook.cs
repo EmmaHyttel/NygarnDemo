488f792f330333fd5e96998111114006e2009ddb
MissMaaai <nmtd10@hotmail.com>
2024-04-23 10:49:38 +0200

    tilføjet mockknittingneedle og crochethook

---
+++ NygarnDemo/MockData/MockCrochetHook.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+
+namespace NygarnDemo.MockData
+{
+    public class MockCrochetHook
+    {
+        private static List<Models.CrochetHook> crochetHookList = new List<Models.CrochetHook>()
+        {
+            new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1),
+            new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1),
+            new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1)
+        };
+    }
+}
--- NygarnDemo/Models/CrochetHook.cs
+++ NygarnDemo/Models/CrochetHook.cs
@@ -7,7 +7,7 @@
         public double Size { get; set; }
 
 
-        public CrochetHook(double price, string name, string description, string brand, int amount, string materiale, double size) : base(price, name, description, brand, amount)
+        public CrochetHook(string materiale, double size, double price, string name, string description, string brand, int amount) : base(price, name, description, brand, amount)
         {
             Materiale = materiale; 
             Size = size;
