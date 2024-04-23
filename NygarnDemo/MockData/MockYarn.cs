b3a7e81ec8142b955ff05ffa773b706dab457bf0
NannaRister <nannarister@hotmail.com>
2024-04-23 10:55:50 +0200

    Underscore i mockdata på yarn og tools

--- NygarnDemo/MockData/MockYarn.cs
+++ NygarnDemo/MockData/MockYarn.cs
@@ -4,7 +4,7 @@ namespace NygarnDemo.MockData
 {
     public class MockYarn
     {
-        private static List<Yarn> yarns = new List<Yarn>() 
+        private static List<Yarn> _yarnProducts = new List<Yarn>() 
         {
             new Yarn("Rød", "27-31 masker = 10 cm", "ca 175 m. pr 50 g.", "Mohair", 50, 79, "MyHair", "Vildt lækkert mohair garn til din næste sweater", "Sandes", 1),
             new Yarn("Beige", "23-26 masker = 10 cm", "ca 195 m. pr 50 g.", "Alpakka", 50, 69, "MaLamaa", "Vildt lækkert alpakka garn til dit næste tørklæde", "PetitKnit", 1),
@@ -13,7 +13,7 @@ namespace NygarnDemo.MockData
 
         public static List<Yarn> GetMockYarn()
         {
-            return yarns;
+            return _yarnProducts;
         }
     }
 }
