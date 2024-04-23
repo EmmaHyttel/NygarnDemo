0c501613102d055f8f76169a6c14855cc4757236
MissMaaai <nmtd10@hotmail.com>
2024-04-23 10:10:57 +0200

    Der er lavet konstruktør på crochet og knittingneedle subklasserne

--- NygarnDemo/Models/KnittingNeedle.cs
+++ NygarnDemo/Models/KnittingNeedle.cs
@@ -1,10 +1,22 @@
 ﻿namespace NygarnDemo.Models
 {
-    public class KnittingNeedle
+    public class KnittingNeedle : Product
     {
-        public string  Materiale { get; set; }
+        public string Materiale { get; set; }
         public string Type { get; set; }
         public double Size { get; set; }
+
+
+        public KnittingNeedle(double price, string name, string description, string brand, int amount, string materiale, string type, double size) : base(price, name, description, brand, amount)
+        {
+            Materiale = materiale;
+            Type = type; 
+            Size = size;
+        }
+
+        public KnittingNeedle() 
+        { 
+        }
     }
 
 }
