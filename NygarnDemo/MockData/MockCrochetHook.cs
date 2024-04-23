8dfedb0d7a6163f18a68d8608a54daf95425e1f8
MissMaaai <nmtd10@hotmail.com>
2024-04-23 10:58:55 +0200

    tilføjet metoder i crochet og knittingneedle

--- NygarnDemo/MockData/MockCrochetHook.cs
+++ NygarnDemo/MockData/MockCrochetHook.cs
@@ -5,11 +5,16 @@ namespace NygarnDemo.MockData
 {
     public class MockCrochetHook
     {
-        private static List<Models.CrochetHook> crochetHookList = new List<Models.CrochetHook>()
+        private static List<Models.CrochetHook> _crochetHooks = new List<Models.CrochetHook>()
         {
             new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1),
             new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1),
             new Models.CrochetHook("Anodiseret aluminium", 2.0, 49.99, "Waves", "Soft grib", "KnitPro", 1)
         };
+
+        public static List<Models.CrochetHook> GetAllCrocketHooks()
+        {
+            return _crochetHooks;
+        }
     }
 }
