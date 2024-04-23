02f42de8fd53bbd4b8dede72aa6d66ce2fe03d3f
MissMaaai <nmtd10@hotmail.com>
2024-04-23 10:01:18 +0200

    oprettet klasserne crochethook og knittingneedle

---
+++ NygarnDemo/Models/KnittingNeedle.cs
@@ -0,0 +1,10 @@
+﻿namespace NygarnDemo.Models
+{
+    public class KnittingNeedle
+    {
+        public string  Materiale { get; set; }
+        public string Type { get; set; }
+        public double Size { get; set; }
+    }
+
+}
