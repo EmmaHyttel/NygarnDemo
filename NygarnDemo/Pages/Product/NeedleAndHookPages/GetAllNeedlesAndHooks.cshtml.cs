f7f76f28aa6816edca904753535df2a94900add8
MissMaaai <nmtd10@hotmail.com>
2024-04-25 11:06:32 +0200

    Rettelser i GetAllNeedlesAndHooks

--- NygarnDemo/Pages/Product/NeedleAndHookPages/GetAllNeedlesAndHooks.cshtml.cs
+++ NygarnDemo/Pages/Product/NeedleAndHookPages/GetAllNeedlesAndHooks.cshtml.cs
@@ -7,23 +7,24 @@ namespace NygarnDemo.Pages.Product.CrochetHookPages
     public class GetAllCrochetHooksModel : PageModel
     {
         private ICrochetHookService _crochetHookService;
-        private IKnittingNeedleService _knittingNeedleService;
+        //private IKnittingNeedleService _knittingNeedleService;
 
 
-        public GetAllCrochetHooksModel(ICrochetHookService crochetHookService, IKnittingNeedleService knittingNeedleService)
+        public GetAllCrochetHooksModel(ICrochetHookService crochetHookService /*IKnittingNeedleService knittingNeedleService*/)
         {
             _crochetHookService = crochetHookService;
-            _knittingNeedleService = knittingNeedleService;
-           
-           
+            //_knittingNeedleService = knittingNeedleService;
         }
         public List<Models.CrochetHook>? CrochetHooks { get; private set; }
-        public List<Models.KnittingNeedle>? KnittingNeedles { get; set; }
+        //public List<Models.KnittingNeedle>? KnittingNeedles { get; set; }
 
         public void OnGet()
         {
-            CrochetHooks = _crochetHookService.GetCrochetHooks(); 
-            KnittingNeedles = _knittingNeedleService.GetKnittingNeedles();
+            CrochetHooks = _crochetHookService.GetCrochetHooks();
         }
+        //public void OnGetKnittingNeedles()
+        //{
+        //    KnittingNeedles = _knittingNeedleService.GetKnittingNeedles();
+        //}
     }
 }
