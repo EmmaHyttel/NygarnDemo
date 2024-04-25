07e67281cb474f3d2c84fc073f6a55537fd89319
Emma Hyttel <emhyttel@gmail.com>
2024-04-25 09:42:56 +0200

    Fjernet alle ToString i modelklasser (minus Yarn)

--- NygarnDemo/Models/Pattern.cs
+++ NygarnDemo/Models/Pattern.cs
@@ -1,26 +1,21 @@
-﻿namespace NygarnDemo.Models
+﻿namespace NygarnDemo.Models;
+
+public class Pattern : Product
 {
-    public class Pattern : Product
+
+    public string Designer { get; set; }
+    public string DifficultyLevel { get; set; }
+
+    public Pattern(string designer, string difficultyLevel, double price, string name, string description, string brand, int amount) : base(price, name, description, brand, amount)
     {
-
-        public string Designer { get; set; }
-        public string DifficultyLevel { get; set; }
-
-        public Pattern(string designer, string difficultyLevel, double price, string name, string description, string brand, int amount) : base(price, name, description, brand, amount)
-        {
-            Designer = designer;
-            DifficultyLevel = difficultyLevel;
-        }
-
-        public Pattern(double price, string name, string description, string brand, int amount) : base(price, name, description, brand, amount)
-        {
-            Designer = "default";
-            DifficultyLevel = "default";
-        }
-
-        public override string ToString()
-        {
-            return $"{{{nameof(Designer)}={Designer}, {nameof(DifficultyLevel)}={DifficultyLevel}, {nameof(ProductId)}={ProductId}, {nameof(Price)}={Price}, {nameof(Name)}={Name}, {nameof(Description)}={Description}, {nameof(Brand)}={Brand}, {nameof(Amount)}={Amount}}}";
-        }
+        Designer = designer;
+        DifficultyLevel = difficultyLevel;
     }
+
+    public Pattern(double price, string name, string description, string brand, int amount) : base(price, name, description, brand, amount)
+    {
+        Designer = "default";
+        DifficultyLevel = "default";
+    }
+
 }
