52684a3d04a74b72b5286cd799f108fc6d84e7eb
MissMaaai <nmtd10@hotmail.com>
2024-04-25 10:44:16 +0200

    tilføjet knittingneedle service, Iknittingneedle i GetAllNeedleAndHooks

--- NygarnDemo/Services/ProductServices/KnittingNeedleService.cs
+++ NygarnDemo/Services/ProductServices/KnittingNeedleService.cs
@@ -1,6 +1,21 @@
-﻿namespace NygarnDemo.Services.ProductServices
+﻿using NygarnDemo.MockData;
+using NygarnDemo.Models;
+using NygarnDemo.Services.Interfaces;
+
+namespace NygarnDemo.Services.ProductServices
 {
-    public class KnittingNeedleService
+    public class KnittingNeedleService : IKnittingNeedleService
     {
+        public List<KnittingNeedle> _knittingNeedle { get; set; }
+
+        public KnittingNeedleService()
+        {
+            _knittingNeedle = MockKnittingNeedle.GetAllKnittingNeedles();
+        }
+
+        public List<KnittingNeedle> GetKnittingNeedles()
+        {
+            return _knittingNeedle;
+        }
     }
 }
