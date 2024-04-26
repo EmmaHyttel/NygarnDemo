15dc5392794116fb9e6d080f1d96d981935d4f2b
NannaRister <nannarister@hotmail.com>
2024-04-26 13:40:17 +0200

    Enum filter på yarn, lige nu kører den på default muligheden

---
+++ NygarnDemo/Enums/Color.cs
@@ -0,0 +1,19 @@
+﻿namespace NygarnDemo.Enums
+{
+    public enum Color
+    {
+        Rød, 
+        Grøn,
+        Sort,
+        Hvid,
+        Gul,
+        Blå,
+        Lilla,
+        Lyserød,
+        Orange,
+        Beige, 
+        Brun,
+        Grå
+    }
+   
+}
