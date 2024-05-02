70a6b1c16ac4c731975047be4cbbd3efce1f2e7b
MissMaaai <nmtd10@hotmail.com>
2024-05-02 11:15:56 +0200

    Oprettet person og Person i models og adminPage

---
+++ NygarnDemo/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace NygarnDemo.Models
+{
+    [Authorize(Roles = "admin")]
+    public class Admin : Person
+    {
+
+        public Admin(int personId, string name, string lastName, string password, string address, string phone) : base(personId, name, lastName, password, address, phone)
+        {
+        }
+    }
+}
---
+++ NygarnDemo/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace NygarnDemo.Models
+{
+    public class Person
+    {
+
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public Person(int personId, string name, string lastName, string password, string address, string phone)
+        {
+
+        }
+        
+    }
+}
