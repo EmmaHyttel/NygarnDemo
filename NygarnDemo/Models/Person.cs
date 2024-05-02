4b32dbd8919355ecbfe2113f6f14453af2c9a0f2
MissMaaai <nmtd10@hotmail.com>
2024-05-02 11:52:13 +0200

    tilføjet person properties, hvor user og admin arver fra

--- NygarnDemo/Models/Person.cs
+++ NygarnDemo/Models/Person.cs
@@ -1,19 +1,63 @@
-﻿namespace NygarnDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace NygarnDemo.Models
 {
     public class Person
     {
 
         public int PersonId { get; set; }
+
+        [Display(Name = "Skriv et ønskede brugernavn")]
+        [Required(ErrorMessage = "Brugernavn må max indeholde 50 tegn")]   //brugernavn taget - throw exception. 
+        [StringLength(30)]
+        public string UserName { get; set; }
+
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Mangler fornavn")]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Display(Name = "Efternavn")]
+        [Required(ErrorMessage = "Mangler efternavn")]
+        [StringLength(100)]
         public string LastName { get; set; }
+
+        [Display(Name = "Skriv ønskede kodeord")]
+        [Required(ErrorMessage = "Skriv kodeord max 50 tegn")]
+        [StringLength(50)]
         public string Password { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Mangler adresse")]
+        [StringLength(100)]
         public string Address { get; set; }
+
+        [Display(Name = "Telefon")]
+        [Required(ErrorMessage = "Telefon nummer max 8 tegn")]
+        [StringLength(8)]
         public string Phone { get; set; }
 
-        public Person(int personId, string name, string lastName, string password, string address, string phone)
+        public Person(int personId, string userName, string name, string lastName, string password, string address, string phone)
         {
-
+            UserName = userName;
+            Name = name;
+            LastName = lastName;
+            Password = password;
+            Address = address;
+            Phone = phone;
         }
-        
+
+        public Person()
+        {
+            UserName = ""; 
+            Name = "";
+            LastName = "";
+            Password = "";
+            Address = "";
+            Phone = "";
+        }
     }
 }
---
+++ NygarnDemo/Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace NygarnDemo.Models
+{
+    public class User : Person
+    {
+        private string WishList { get; set; }   
+
+
+        public User(int personId, string userName, string name, string lastName, string password, string address, string phone) : base(personId, userName, name, lastName, password, address, phone)
+        {
+            
+            Name = name;
+            LastName = lastName;
+            Password = password;
+            Address = address;
+            Phone = phone; 
+        }
+    }
+}
