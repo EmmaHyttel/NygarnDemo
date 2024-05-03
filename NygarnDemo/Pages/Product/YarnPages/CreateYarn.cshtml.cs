7097bae4c0506173041e61090a696191ef70cd11
Emma Hyttel <emhyttel@gmail.com>
2024-05-03 10:55:50 +0200

    Lavet ny createYarn page, hvor formen ser korrekt ud, den smider dog en fejl meddelelse når jeg forsøger at tilgå CreateYarn pagen

---
+++ NygarnDemo/Pages/Product/YarnPages/CreateYarn.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NygarnDemo.Models;
+using NygarnDemo.Services.ProductServices;
+
+namespace NygarnDemo.Pages.Product.YarnPages
+{
+	public class CreateYarnModel : PageModel
+	{
+		private YarnService _yarnService;
+
+		[BindProperty]
+		public Yarn Yarn { get; set; }
+
+		public CreateYarnModel(YarnService yarnService)
+		{
+			_yarnService = yarnService;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+		public IActionResult OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			_yarnService.AddYarn(Yarn);
+			return RedirectToPage("GetAllYarnProducts");
+		}
+	}
+}
