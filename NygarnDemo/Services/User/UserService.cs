bb80f4b0e3e180d9705bb93b8afbc133528744dd
MissMaaai <nmtd10@hotmail.com>
2024-05-03 10:41:34 +0200

    Tilføjet addsingleton, userservice og Iservice

---
+++ NygarnDemo/Services/User/UserService.cs
@@ -0,0 +1,21 @@
+﻿using NygarnDemo.MockData;
+using NygarnDemo.Models;
+
+namespace NygarnDemo.Services.User
+{
+    public class UserService : IUserService
+    {
+        public List<Models.User> Users { get; set; }
+
+
+        public UserService()
+        {
+            Users = MockUser.GetAllUsers();
+
+        }
+        public List<Models.User> GetAllUsers()
+        {
+            return Users;
+        }
+    }
+}
