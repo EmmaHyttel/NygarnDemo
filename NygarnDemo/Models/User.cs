fafb5d35ee0aa131fd5ac2a1f30b9953dc59f17c
NannaRister <nannarister@hotmail.com>
2024-05-06 10:37:10 +0200

    Properties i user konstruktøren er ikke udkommenteret længere

--- NygarnDemo/Models/User.cs
+++ NygarnDemo/Models/User.cs
@@ -9,11 +9,11 @@ namespace NygarnDemo.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string UserName { get; set; }
         public string Password { get; set; }
-        //public User(int personId, string userName, string name, string lastName, string password, string address, string phone, string email) : base(personId, userName, name, lastName, password, address, phone, email)
-        //{
-        //    UserName = userName;
-        //    Password = password;
-        //}
+        public User(int personId, string userName, string name, string lastName, string password, string address, string phone, string email) : base(personId, userName, name, lastName, password, address, phone, email)
+        {
+            UserName = userName;
+            Password = password;
+        }
 
         public User(string userName, string password)
         {
