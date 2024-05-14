e1e6a480abeaf1d2c3a5cc9f4921f8b7aafee2dc
MissMaaai <nmtd10@hotmail.com>
2024-05-14 12:25:16 +0200

    Rettet i createknittingneedle, der var et s for meget på GetAllNeedlesAndHooks knappen der omdigerer til at createknittingneedle.

--- NygarnDemo/Pages/Product/NeedleAndHookPages/CreateKnittingNeedle.cshtml.cs
+++ NygarnDemo/Pages/Product/NeedleAndHookPages/CreateKnittingNeedle.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NygarnDemo.Models;
+using NygarnDemo.Services.Interfaces;
 
 namespace NygarnDemo.Pages.Product.NeedleAndHookPages
 {
-    public class CreateKnittingNeedleModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class CreateKnittingNeedleModel : PageModel
+	{
+		private IKnittingNeedleService _knittingNeedleService;
+		private IWebHostEnvironment _webHostEnvironment;
+
+
+		[BindProperty]
+		public KnittingNeedle knittingNeedle { get; set; }
+
+		public CreateKnittingNeedleModel(IKnittingNeedleService knittingNeedleService, IWebHostEnvironment webHost)
+		{
+			_knittingNeedleService = knittingNeedleService;
+			_webHostEnvironment = webHost;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			await _knittingNeedleService.AddKnittingNeedleAsync(knittingNeedle);
+			return RedirectToPage("GetAllNeedlesAndHooks");
+		}
+	}
 }
+
