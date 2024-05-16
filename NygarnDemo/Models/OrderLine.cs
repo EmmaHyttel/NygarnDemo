77b1d7e7964eec5de582da1186f1c5420cc1f045
NannaRister <nannarister@hotmail.com>
2024-05-16 11:50:07 +0200

    Order og orderlines

---
+++ NygarnDemo/Models/OrderLine.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NygarnDemo.Models
+{
+    public class OrderLine
+    {
+        [Required]
+        public int OrderId { get; set; }
+
+        public Order Order { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public OrderLine()
+        {
+        }
+
+        public OrderLine(int orderId, Order order, int productId, Product product)
+        {
+            OrderId = orderId;
+            Order = order;
+            ProductId = productId;
+            Product = product;
+        }
+    }
+}
