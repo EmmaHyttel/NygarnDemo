31fc4ecc0193d656ef8bad930834f96dd13c32d1
Emma Hyttel <emhyttel@gmail.com>
2024-05-16 11:20:14 +0200

    start op på admin i person tabel

---
+++ NygarnDemo/EFDbContext/PersonDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NygarnDemo.Models;
+
+namespace NygarnDemo.EFDbContext
+{
+    public class PersonDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=mssql17.unoeuro.com;
+							Initial Catalog=nygarndemo_dk_db_projekt;
+							User ID=nygarndemo_dk;
+							Password=drHmxBw4p6nkFcha2Abt;
+							Connect Timeout=30;Encrypt=True;
+							Trust Server Certificate=True;
+							Application Intent=ReadWrite;
+							Multi Subnet Failover=False;
+							Connect Timeout=30; 
+							Encrypt=False"
+            );
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person")
+                .HasData(
+                    new Person()
+                    {
+                        Id = 1,
+                        UserName = "admin",
+                        Name = "NEM",
+                        LastName = "HARD",
+                        Password = "secretzz",
+                        Address = "En sej vej",
+                        Phone = "69696969",
+                        Email = "nem@hard.com"
+                    });
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
