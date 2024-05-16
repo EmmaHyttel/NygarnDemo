c83025be53886555481dba1bea37cfb552743cb1
Emma Hyttel <emhyttel@gmail.com>
2024-05-16 11:48:49 +0200

    Person er ude

--- NygarnDemo/Models/User.cs
+++ NygarnDemo/Models/User.cs
@@ -3,23 +3,68 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NygarnDemo.Models
 {
-    public class User : Person
+    public class User
     {
-        //public string UserName { get; set; }
-        //public string Password { get; set; }
-        public List<Yarn> YarnWishList { get; set; }
-        public User(string userName, string name, string lastName, string password, string address,
-            string phone, string email) : base(userName, name, lastName, password, address, phone, email)
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+        [Key]
+        [Display(Name = "Skriv et ønskede brugernavn")]
+        [Required(ErrorMessage = "Brugernavn må max indeholde 50 tegn")]   //brugernavn taget - throw exception. 
+        [StringLength(30)]
+        public string UserName { get; set; }
+
+        [Display(Name = "Fornavn")]
+        [Required(ErrorMessage = "Mangler fornavn")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name = "Efternavn")]
+        [Required(ErrorMessage = "Mangler efternavn")]
+        [StringLength(100)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Skriv ønskede kodeord")]
+        [Required(ErrorMessage = "Skriv kodeord minimum 8 tegn")]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+        [Display(Name = "Adresse")]
+        [Required(ErrorMessage = "Mangler adresse")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Display(Name = "Telefon")]
+        [Required(ErrorMessage = "Telefon nummer max 8 tegn")]
+        [StringLength(8)]
+        public string Phone { get; set; }
+
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Mangler mail")]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        public User(string userName, string name, string lastName, string password, string address, string phone, string email)
         {
-            //YarnWishList = new List<Yarn>();
-            //UserName = userName;
-            //Password = password;
+            UserName = userName;
+            Name = name;
+            LastName = lastName;
+            Password = password;
+            Address = address;
+            Phone = phone;
+            Email = email;
         }
 
         public User()
         {
-            //UserName = userName;
-            //Password = password;
+            //UserName = ""; 
+            //Name = "";
+            //LastName = "";
+            //Password = "";
+            //Address = "";
+            //Phone = "";
+            //Email = "";
         }
     }
 }
