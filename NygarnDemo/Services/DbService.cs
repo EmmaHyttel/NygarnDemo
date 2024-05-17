e19b54e7ce0e38cfa7aa9964059839d12ee78f75
Emma Hyttel <emhyttel@gmail.com>
2024-05-17 09:47:43 +0200

    Lavet UserDbService

--- NygarnDemo/Services/DbService.cs
+++ NygarnDemo/Services/DbService.cs
@@ -8,37 +8,8 @@ namespace NygarnDemo.Services
     {
      
     
-        #region User
-        public async Task<List<Models.User>> GetUsers()
-        {
-            using (var context = new UserDbContext())
-            {
-                return await context.UserTable.ToListAsync();
-            }
-        }
-        public async Task AddUser(Models.User user)
-        {
-            using (var context = new UserDbContext())
-            {
-                context.UserTable.Add(user);
-                context.SaveChanges();
-            }
-        }
-
-        public async Task SaveUsers(List<Models.User> users)
-        {
-            using (var context = new UserDbContext())
-            {
-                foreach (Models.User user in users)
-                {
-                    context.UserTable.Add(user);
-                    context.SaveChanges();
-                }
-
-                context.SaveChanges();
-            }
-        }
-		#endregion
+       
+       
 		#region KnittingNeedle
 		public async Task<List<KnittingNeedle>> GetKnittingNeedles()
         {
---
+++ NygarnDemo/Services/DbServices/UserDbService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NygarnDemo.EFDbContext;
+
+namespace NygarnDemo.Services.DbServices
+{
+    public class UserDbService
+    {
+        public async Task<List<Models.User>> GetUsers()
+        {
+            using (var context = new UserDbContext())
+            {
+                return await context.UserTable.ToListAsync();
+            }
+        }
+        public async Task AddUser(Models.User user)
+        {
+            using (var context = new UserDbContext())
+            {
+                context.UserTable.Add(user);
+                context.SaveChanges();
+            }
+        }
+
+        public async Task SaveUsers(List<Models.User> users)
+        {
+            using (var context = new UserDbContext())
+            {
+                foreach (Models.User user in users)
+                {
+                    context.UserTable.Add(user);
+                    context.SaveChanges();
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
