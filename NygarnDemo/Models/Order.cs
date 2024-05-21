cd708c5ecde708cd6169312fdea77de40618f163
NannaRister <nannarister@hotmail.com>
2024-05-21 13:38:52 +0200

    Udkommentet order

--- NygarnDemo/Models/Order.cs
+++ NygarnDemo/Models/Order.cs
@@ -5,34 +5,34 @@ namespace NygarnDemo.Models
 {
     public class Order
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int OrderId { get; set; }
+        //    [Key]
+        //    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //    public int OrderId { get; set; }
 
-        [Required]
-        public int Id { get; set; } //Id
+        //    [Required]
+        //    public int Id { get; set; } //Id
 
-        public User User { get; set; }
+        //    public User User { get; set; }
 
-        [Required]
-        public int ProductId { get; set; }
-        public Product Product { get; set; }
+        //    [Required]
+        //    public int ProductId { get; set; }
+        //    public Product Product { get; set; }
 
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-        public DateTime Date { get; set; }
+        //    [DataType(DataType.Date)]
+        //    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        //    public DateTime Date { get; set; }
 
-        public Order()
-        {
-        }
+        //    public Order()
+        //    {
+        //    }
 
-        public Order(User user, Product product)
-        {
-            User = user;
-            Product = product;
-            Date = DateTime.Now;
-        }
-        public virtual ICollection<OrderLine> OrderLines { get; set; }
+        //    public Order(User user, Product product)
+        //    {
+        //        User = user;
+        //        Product = product;
+        //        Date = DateTime.Now;
+        //    }
+        //    public virtual ICollection<OrderLine> OrderLines { get; set; }
 
 
     }
--- NygarnDemo/Models/OrderLine.cs
+++ NygarnDemo/Models/OrderLine.cs
@@ -5,19 +5,19 @@ namespace NygarnDemo.Models
 {
     public class OrderLine
     {
-        [Key, Column(Order = 0)]
-        public int OrderId { get; set; }
+        //[Key, Column(Order = 0)]
+        //public int OrderId { get; set; }
 
-        //[Key, Column(Order = 1)]
-        public int ProductId { get; set; }
+        ////[Key, Column(Order = 1)]
+        //public int ProductId { get; set; }
 
-        [ForeignKey("OrderId")]
-        public virtual Order Order { get; set; }
+        //[ForeignKey("OrderId")]
+        //public virtual Order Order { get; set; }
 
-        [ForeignKey("ProductId")]
-        public virtual Product Product { get; set; }
+        //[ForeignKey("ProductId")]
+        //public virtual Product Product { get; set; }
 
-        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
-        public int Count { get; set; }
+        //[Range(1, int.MaxValue, ErrorMessage = "Please enter a value bigger than {1}")]
+        //public int Count { get; set; }
     }
 }
