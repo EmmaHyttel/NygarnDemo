f08cbe61d810fa71a2d2d627742b66b3aea7ac49
MissMaaai <nmtd10@hotmail.com>
2024-06-04 11:51:16 +0200

    oprettet mappen garninfo og klasser i den

---
+++ NygarnDemo/Pages/GarnInfo/Sandnes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NygarnDemo.Pages.GarnInfo
+{
+    public class SandnesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
