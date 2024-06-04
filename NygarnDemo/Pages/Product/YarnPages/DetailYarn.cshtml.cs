1c7592850aa830e2cdfea9da8371a2a39d467e38
NannaRister <nannarister@hotmail.com>
2024-06-04 23:12:35 +0200

    Detaljer på yarn

--- NygarnDemo/Pages/Product/YarnPages/DetailYarn.cshtml.cs
+++ NygarnDemo/Pages/Product/YarnPages/DetailYarn.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NygarnDemo.Services.Interfaces;
@@ -6,6 +7,7 @@ using System.Security.Claims;
 
 namespace NygarnDemo.Pages.Product.YarnPages
 {
+   
     public class DetailYarnModel : PageModel
     {
 
